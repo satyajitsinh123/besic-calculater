2356fab11771dae53b5e474251de3a09f9279f94
satyajitsinh123 <satyajitsinh63@gamil.com>
2024-05-30 10:06:14 +0530

    besic calculater

---
+++ index.js
@@ -0,0 +1,63 @@
+
+// // qustion 1 Simple Calculator
+
+// let num1 = 10;
+// let num2 = 5;
+// let operator = '+';
+
+// let result;
+
+// if (operator === '+') {
+//   result = num1 + num2;
+// } else if (operator === '-') {
+//   result = num1 - num2;
+// } else if (operator === '*') {
+//   result = num1 * num2;
+// } else if (operator === '/') {
+//   result = num1 / num2;
+// } else {
+//   result = 'Invalid operator';
+// }
+
+// console.log(`Result: ${result}`);
+
+
+
+
+
+// // qustion 2  Swap Two Variables
+
+// let a = 5;
+// let b = 10;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log(`a: ${a}, b: ${b}`);
+
+
+
+
+
+// // qustion 3
+
+// let num = 7;
+// let square = num * num;
+
+// console.log(`Square of ${num} is ${square}`);
+
+
+
+
+
+
+// // qustion 4 third angle of a right triangle if two other angles
+
+// let angleA = 30; 
+// let angleB = 60; 
+// let angleC = 180 - (angleA + angleB);
+
+// console.log(`Third angle: ${angleC} degrees`);
+
+  
